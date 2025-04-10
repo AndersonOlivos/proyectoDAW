45b9b4ab4dec86d131a70659e4bab2d3660f6e54
Anderson <anderolivosg@gmail.com>
2025-04-10 16:56:07 +0200

    Configuracion Base de Datos

--- proyectoDAW/settings.py
+++ proyectoDAW/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'proyectoDAW.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XE',
+        'USER': 'C##ANDER',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '1521',
+        'OPTIONS': {
+            'threaded': True,
+        }
     }
 }
 
